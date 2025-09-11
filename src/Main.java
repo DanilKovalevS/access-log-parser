90423221c163e95e3c8f705410fcb35336f6c87f
Kovalev Daniil <megadan2013@mail.ru>
2025-09-11 11:27:23 +0300

    Подписал рез-т

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int x = new Scanner(System.in)  .nextInt();
-        int y = new Scanner(System.in)  .nextInt();
+        int x = new Scanner(System.in) .nextInt();
+        int y = new Scanner(System.in) .nextInt();
         double b = (double) x / y;
-        System.out.println(x+y);
-        System.out.println(x-y);
-        System.out.println(x*y);
-        System.out.println(b);
+        System.out.println("Сумма:"+(x+y));
+        System.out.println("Разность:"+(x-y));
+        System.out.println("Произведение:"+(x*y));
+        System.out.println("Частное:"+b);
     }
 }
